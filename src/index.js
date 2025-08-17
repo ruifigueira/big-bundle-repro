72e49dff5018f042865f1a92473542df2a882dd0
Rui Figueira <rfigueira@cloudflare.com>
2025-08-17 11:49:34 +0100

    Uncaught SyntaxError: Invalid or unexpected token

--- src/index.js
+++ src/index.js
@@ -1,12 +1,5 @@
-import base64Content from './Hubble_M31Mosaic_2025_10552x2468_jpg_base64.txt';
-
 export default {
   async fetch() {
-    const img = Buffer.from(base64Content, 'base64');
-    return new Response(img, {
-      headers: {
-        'Content-Type': 'image/jpeg'
-      }
-    });
+    return new Response('Hello World');
   }
 };
