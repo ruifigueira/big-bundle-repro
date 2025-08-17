bd090d26d8790e36583e6d27fc198b775f78ff42
Rui Figueira <rfigueira@cloudflare.com>
2025-08-17 11:34:54 +0100

    Simplified even further
    - no durable object needed, only `wrangler dev --remote`
    - latest released wrangler version has the same issue (I also tried an older one (4.23.0), and it has the same issue too)

--- README.md
+++ README.md
@@ -11,14 +11,8 @@ npx wrangler dev --remote
 
 Perform a request by pressing `b`.
 
-It should fail, and in the terminal it should output something like:
+In the terminal, we should get an internal error:
 
 ```
-[wrangler:info] Ready on http://localhost:8787
-Script modified; context reset.
-[wrangler:info] GET / 500 Internal Server Error (335ms)
-✘ [ERROR] Uncaught Error: internal error; reference = khmb50r2k3i8knrpai3fac5r
-
-
-✘ [ERROR] Uncaught Error: internal error; reference = khmb50r2k3i8knrpai3fac5r
+✘ [ERROR] Uncaught Error: internal error; reference = mcfk3bq8fdqbicj80ts7vsq9
 ```
--- src/index.js
+++ src/index.js
@@ -1,12 +1,6 @@
-import { DurableObject } from 'cloudflare:workers';
-
 import base64Content from './Hubble_M31Mosaic_2025_10552x2468_jpg_base64.txt';
 
-export class MyDurableObject extends DurableObject {
-  constructor(state, env) {
-    super(state, env);
-  }
-
+export default {
   async fetch() {
     const img = Buffer.from(base64Content, 'base64');
     return new Response(img, {
@@ -14,13 +8,5 @@ export class MyDurableObject extends DurableObject {
         'Content-Type': 'image/jpeg'
       }
     });
-  }
-}
-
-export default {
-  async fetch(request, env) {
-    const id = env.MY_DURABLE_OBJECT.idFromName(`MY_DURABLE_OBJECT`);
-    const stub = env.MY_DURABLE_OBJECT.get(id);
-    return await stub.fetch(request);
   }
 };
